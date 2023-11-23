74e8300a61bd651e2bcf707e1b566bc3ced23329
ajistrying <ajistrying@gmail.com>
2023-11-22 21:44:16 -0500

    implement builtwith functionality

--- pkg/opendb/builtwith.go
+++ pkg/opendb/builtwith.go
@@ -30,14 +30,89 @@ package opendb
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	httpUtils "github.com/edoardottt/scilla/internal/http"
 )
 
+type builtWithResultWrapper struct {
+	Results []struct {
+		Result struct {
+			SpendHistory []struct {
+				D int64 `json:"D"`
+				S int   `json:"S"`
+			} `json:"SpendHistory"`
+			IsDB  string `json:"IsDB"`
+			Spend int    `json:"Spend"`
+			Paths []struct {
+				Technologies []struct {
+					Name          string   `json:"Name"`
+					Description   string   `json:"Description"`
+					Link          string   `json:"Link"`
+					Tag           string   `json:"Tag"`
+					FirstDetected int64    `json:"FirstDetected"`
+					LastDetected  int64    `json:"LastDetected"`
+					IsPremium     string   `json:"IsPremium"`
+					Categories    []string `json:"Categories,omitempty"`
+					Parent        string   `json:"Parent,omitempty"`
+				} `json:"Technologies"`
+				FirstIndexed int64  `json:"FirstIndexed"`
+				LastIndexed  int64  `json:"LastIndexed"`
+				Domain       string `json:"Domain"`
+				URL          string `json:"Url"`
+				SubDomain    string `json:"SubDomain"`
+			} `json:"Paths"`
+		} `json:"Result"`
+		Meta struct {
+			Majestic    int      `json:"Majestic"`
+			Umbrella    int      `json:"Umbrella"`
+			Vertical    string   `json:"Vertical"`
+			Social      []string `json:"Social"`
+			CompanyName string   `json:"CompanyName"`
+			Telephones  []string `json:"Telephones"`
+			Emails      []any    `json:"Emails"`
+			City        string   `json:"City"`
+			State       string   `json:"State"`
+			Postcode    string   `json:"Postcode"`
+			Country     string   `json:"Country"`
+			Names       []struct {
+				Name  string `json:"Name"`
+				Type  int    `json:"Type"`
+				Email string `json:"Email"`
+			} `json:"Names"`
+			ARank int `json:"ARank"`
+			QRank int `json:"QRank"`
+		} `json:"Meta"`
+		Attributes struct {
+			Employees    int `json:"Employees"`
+			MJRank       int `json:"MJRank"`
+			MJTLDRank    int `json:"MJTLDRank"`
+			RefSN        int `json:"RefSN"`
+			RefIP        int `json:"RefIP"`
+			Followers    int `json:"Followers"`
+			Sitemap      int `json:"Sitemap"`
+			GTMTags      int `json:"GTMTags"`
+			QubitTags    int `json:"QubitTags"`
+			TealiumTags  int `json:"TealiumTags"`
+			AdobeTags    int `json:"AdobeTags"`
+			CDimensions  int `json:"CDimensions"`
+			CGoals       int `json:"CGoals"`
+			CMetrics     int `json:"CMetrics"`
+			ProductCount int `json:"ProductCount"`
+		} `json:"Attributes"`
+		FirstIndexed int64  `json:"FirstIndexed"`
+		LastIndexed  int64  `json:"LastIndexed"`
+		Lookup       string `json:"Lookup"`
+		SalesRevenue int    `json:"SalesRevenue"`
+	} `json:"Results"`
+	Errors []any `json:"Errors"`
+	Trust  any   `json:"Trust"`
+}
+
 // Builtwith retrieves from the url below some known subdomains.
 func BuiltWithSubdomains(target, apikey string, plain bool) []string {
-	var result []string
+	result := []string{}
 
 	if !plain {
 		fmt.Println("Pulling data from BuiltWith")
@@ -48,27 +123,35 @@ func BuiltWithSubdomains(target, apikey string, plain bool) []string {
 	}
 
 	fetchURL := fmt.Sprintf(
-		"https://api.builtwith.com/v21/api.json?LOOKUP=%s&KEY=%s",
-		target, apikey,
+		"https://api.builtwith.com/v21/api.json?&KEY=%s&LOOKUP=%s",
+		apikey, target,
 	)
-
-	wrapper := struct {
-		Subdomains []string `json:"subdomains"`
-	}{}
 
 	resp, err := client.Get(fetchURL)
 	if err != nil {
 		return result
 	}
-	defer resp.Body.Close()
-	dec := json.NewDecoder(resp.Body)
 
-	err = dec.Decode(&wrapper)
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return result
 	}
 
-	result = append(result, wrapper.Subdomains...)
+	var bwrWrapper builtWithResultWrapper
+	if err := json.Unmarshal(body, &bwrWrapper); err != nil {
+		return result
+	}
+
+	for _, elem := range bwrWrapper.Results[0].Result.Paths {
+		if elem.SubDomain == "" {
+			continue
+		}
+		
+		fullDomain := elem.SubDomain + "." + target
+		result = append(result, fullDomain)
+	}
 
 	return result
 }
